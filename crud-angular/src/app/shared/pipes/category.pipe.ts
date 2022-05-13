5adb16afa905a0161d527b976f5165ac47dad1bc
Paulo Henrique <rick.souza97@gmail.com>
2022-05-13 17:19:44 -0300

    organizando categorias

--- crud-angular/src/app/shared/pipes/category.pipe.ts
+++ crud-angular/src/app/shared/pipes/category.pipe.ts
@@ -11,6 +11,7 @@ export class CategoryPipe implements PipeTransform {
       case 'Front-end': return 'code';
       case 'Back-end': return 'computer';
       case 'Mobile': return 'smartphone';
+      case 'Devops': return 'settings_suggest';
     }
     return 'remove';
   }
--- crud-spring/src/main/resources/data.sql
+++ crud-spring/src/main/resources/data.sql
@@ -5,4 +5,8 @@ insert into courses (category, name) values ('Front-end', 'ReactJS');
 insert into courses (category, name) values ('Front-end', 'JavaScript');
 insert into courses (category, name) values ('Back-end', 'Cobol');
 insert into courses (category, name) values ('Back-end', 'C');
+insert into courses (category, name) values ('Devops', 'Docker');
+insert into courses (category, name) values ('Devops', 'Kubernetes');
+insert into courses (category, name) values ('Devops', 'Vagrant');
+insert into courses (category, name) values ('Devops', 'Nagios');
 
